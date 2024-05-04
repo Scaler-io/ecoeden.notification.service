207571b662a695597ca2b1536cf5dce61c06c08d
sharthak95 <sharthakmallik@gmail.com>
2024-05-05 00:52:12 +0530

    handles user invitation email

--- src/Ecoeden.Notification.Service/EventBus/Consumers/UserInvitationSentConsumer.cs
+++ src/Ecoeden.Notification.Service/EventBus/Consumers/UserInvitationSentConsumer.cs
@@ -6,72 +6,80 @@ using Ecoeden.Notification.Service.Extensions;
 using Ecoeden.Notification.Service.Models.Constants;
 using Ecoeden.Notification.Service.Models.Notifications;
 using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Serilog;
 
+namespace Ecoeden.Notification.Service.EventBus.Consumers;
 
-namespace Ecoeden.Notification.Service.EventBus.Consumers
+public sealed class UserInvitationSentConsumer : IConsumer<UserInvitationSent>
 {
-    public sealed class UserInvitationSentConsumer : IConsumer<UserInvitationSent>
+    private readonly ILogger _logger;
+    private readonly EmailTemplates _emailTemplates;
+    private readonly NotificationDbContext _dbContext;
+    private readonly IConfiguration _configuration;
+
+    public UserInvitationSentConsumer(ILogger logger,
+        NotificationDbContext dbContext,
+        IOptions<EmailTemplates> emailTemplates,
+        IConfiguration configuration)
     {
-        private readonly ILogger _logger;
-        private readonly EmailTemplates _emailTemplates;
-        private readonly NotificationDbContext _dbContext;
+        _logger = logger;
+        _emailTemplates = emailTemplates.Value;
+        _dbContext = dbContext;
+        _configuration = configuration;
+    }
 
-        public UserInvitationSentConsumer(ILogger logger, 
-            NotificationDbContext dbContext,
-            IOptions<EmailTemplates> emailTemplates)
+    public async Task Consume(ConsumeContext<UserInvitationSent> context)
+    {
+        _logger.Here().MethodEnterd();
+        _logger.Here()
+            .ForContext("MessageId", context.MessageId)
+            .WithCorrelationId(context.Message.CorrelationId)
+            .Information("Message processing started for event {Event}", typeof(UserInvitationSent).Name);
+
+        try
         {
-            _logger = logger;
-            _emailTemplates = emailTemplates.Value;
-            _dbContext = dbContext;
-        }
-
-        public async Task Consume(ConsumeContext<UserInvitationSent> context)
-        {
-            _logger.Here().MethodEnterd();
-            _logger.Here()
-                .ForContext("MessageId", context.MessageId)
-                .WithCorrelationId(context.Message.CorrelationId)
-                .Information("Message processing started for event {Event}", typeof(UserInvitationSent).Name);
-
-            try
+            var identityBaseUrl = _configuration["InfrastructureSettings:identityBaseUrl"];
+            NotificationHistory notificationHistory = new()
             {
-                NotificationHistory notificationHistory = new()
-                {
-                    Subject = EmailSubjects.UserInvitation,
-                    Data = GetEmailData(context.Message),
-                    CorrelationId = context.Message.CorrelationId,
-                    IsPublished = false,
-                    TemplateName = _emailTemplates.UserInivite,
-                    RecipientEmail = context.Message.Email
-                };
-
-                _dbContext.Add(notificationHistory);
-                await _dbContext.SaveChangesAsync();
-
-                _logger.Here()
-                    .WithCorrelationId(context.Message.CorrelationId)
-                    .Information("Notification history updated");
-            }
-            catch(Exception ex)
-            {
-                _logger.Here()
-                    .WithCorrelationId(context.Message.CorrelationId)
-                    .Error(ex.Message);
-            }
-        }
-
-        private static string GetEmailData(UserInvitationSent message)
-        {
-            List<TemplateFields> templateFields = new()
-            {
-                new("[firstname]", message.FirstName),
-                new("[lastname]", message.LastName),
-                new("[acceptanceLink]", "http://ecoeden.com/")
+                Subject = EmailSubjects.UserInvitation,
+                Data = GetEmailData(context.Message, identityBaseUrl),
+                CorrelationId = context.Message.CorrelationId,
+                IsPublished = false,
+                TemplateName = _emailTemplates.UserInivite,
+                RecipientEmail = context.Message.Email
             };
-            return JsonConvert.SerializeObject(templateFields);
+
+            _dbContext.Add(notificationHistory);
+            await _dbContext.SaveChangesAsync();
+
+            _logger.Here()
+                .WithCorrelationId(context.Message.CorrelationId)
+                .Information("Notification history updated");
         }
+        catch(Exception ex)
+        {
+            _logger.Here()
+                .WithCorrelationId(context.Message.CorrelationId)
+                .Error(ex.Message);
+        }
+    }
+
+    private static string GetEmailData(UserInvitationSent message, string identityUrl)
+    {
+        JObject obj = JObject.Parse(message.AdditionalProperties.ToString());
+        string token = obj["token"].Value<string>();
+
+        Uri url = new Uri($"{identityUrl}/account/emailverification?userId={message.UserId}&token={token}");
+        List<TemplateFields> templateFields = new()
+        {
+            new("[firstname]", message.FirstName),
+            new("[lastname]", message.LastName),
+            new("[acceptanceLink]", url.ToString())
+        };
+        return JsonConvert.SerializeObject(templateFields);
     }
 }
--- src/Ecoeden.Notification.Service/EventBus/Contracts/NotificationEvent.cs
+++ src/Ecoeden.Notification.Service/EventBus/Contracts/NotificationEvent.cs
@@ -6,6 +6,7 @@ namespace Ecoeden.Notification.Service.EventBus.Contracts
     {
         public DateTime CreatedAt { get; set; }
         public string CorrelationId { get; set; }
+        public object AdditionalProperties { get; set; }
         public abstract NotificationType NotificationType { get; set; }
     }
 }
